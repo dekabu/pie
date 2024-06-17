d284d283e0048889f93df38fa91ae3fc1fe27f13
Denis <dyonya0.0@gmail.com>
2024-06-17 06:05:48 +0400

    0

--- src/lex/lex.c
+++ src/lex/lex.c
@@ -6,7 +6,12 @@
 	if (ret.len == 0) \
 		ret.tokens = malloc(sizeof(pie_Token)); \
 	else if (pie_ispow2(ret.len)) \
-		ret.tokens = relloc(ret.tokens, 2 * ret.len * sizeof(pie_Token))
+		ret.tokens = realloc(ret.tokens, 2 * ret.len * sizeof(pie_Token))
+
+#define add(t) { \
+	mem; \
+	ret.tokens[ret.len++].token = t; \
+}
 
 pie_TokenList pie_parse(char* s)
 {
@@ -20,8 +25,34 @@ pie_TokenList pie_parse(char* s)
 			case ' ':
 				++i;
 				break;
+			case '.':
+				switch (s[++i]) {
+					case '.':
+						if (s[++i] == '.') {
+							add(pie_token_ellipses)
+							++i;
+						}
+						else
+							add(pie_token_double_dot)
+						break;
+					case '=':
+						add(pie_token_dot_equal)
+						++i;
+						break;
+					default:
+						add(pie_token_dot)
+						break;
+				}
+				break;
+			case ',':
+				add(pie_token_comma)
+				++i;
+				break;
 		}
 	}
 
 	return ret;
 }
+
+#undef mem
+#undef add
